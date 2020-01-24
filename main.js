b2f2988b5a5e72d7ae0c9ab5d86eb81bbcae64f5
Bibek Chapagain <bibekchapagain27>
2020-01-24 19:46:44 +0545

    learning about JS datatypes

--- main.js
+++ main.js
@@ -9,3 +9,17 @@
 // const age = 30;
 // //age = 32; //gives error... constant values can't be changed
 // console.log(age);
+
+
+//datatypes
+//primitive data types.. strings, numbers, boolean, null, undefines
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof rating);
