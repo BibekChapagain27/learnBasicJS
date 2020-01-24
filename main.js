487d3c06252af4e10d75e5d6686ca0bf36f4f2be
Bibek Chapagain <bibekchapagain27>
2020-01-24 19:37:37 +0545

    learning about JS variables

---
+++ main.js
@@ -0,0 +1,11 @@
+//variables
+//let, const
+
+// let age = 30;
+// age = 32;
+// console.log(age);
+
+
+// const age = 30;
+// //age = 32; //gives error... constant values can't be changed
+// console.log(age);
