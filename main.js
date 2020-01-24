6c7b4693e6c6ec860b032e6c8e7017008af9c0a0
Bibek Chapagain <bibekchapagain27>
2020-01-24 23:51:05 +0545

    conditionals in JS

--- main.js
+++ main.js
@@ -126,23 +126,23 @@
     
 
 
-const todos = [
-    {
-        id: 1,
-        text: 'Take out trash',
-        isCompleted: true
-    },
-    {
-        id: 2,
-        text: 'Meeting with boss',
-        isCompleted: true
-    },
-    {
-        id: 3,
-        text: 'Dentist appointment',
-        isCompleted: false
-    }
-];
+// const todos = [
+//     {
+//         id: 1,
+//         text: 'Take out trash',
+//         isCompleted: true
+//     },
+//     {
+//         id: 2,
+//         text: 'Meeting with boss',
+//         isCompleted: true
+//     },
+//     {
+//         id: 3,
+//         text: 'Dentist appointment',
+//         isCompleted: false
+//     }
+// ];
 
 //forEach, map, filter
 
@@ -176,3 +176,46 @@ const todos = [
 // });
 // console.log(todoCompleted);
 
+
+
+//Conditionals
+
+
+// const x = 7;
+// const y = 11;
+
+// // if(x === 10){
+// //     console.log('x is 10');
+    
+// //  } else if(x>10){
+// //      console.log('x is greater than 10');
+     
+// //  } else{
+// //     console.log('x is less than 10');
+    
+// // }
+
+// if(x>5 && y>10){
+//     console.log('x is greater than 5 and y is greater than 10');
+    
+// }
+
+
+//Ternary Operator
+
+// const x = 10;
+
+// const color = x > 10 ? 'red' : 'blue';
+
+// switch(color){
+//     case 'red':
+//         console.log('color is red');
+//         break;
+//     case 'blue':
+//         console.log('color is blue');
+//         break;
+//     default:
+//         console.log('color is not red or blue');
+//         break;
+        
+// }
