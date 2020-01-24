6bfee37afe88ab9acb48324f6c4b008acf78f9b8
Bibek Chapagain <bibekchapagain27>
2020-01-24 21:05:54 +0545

    object literals in JS

--- main.js
+++ main.js
@@ -59,3 +59,25 @@
 // console.log(fruits.indexOf('oranges')); //index of 
 
 
+//object literals
+
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies:['music', 'movies', 'sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    } 
+}
+
+// console.log(person.firstName, person.lastName);
+// console.log(person.hobbies[1]);
+// console.log(person.address.city);
+
+person.email = 'john@gmail.com';
+
+console.log(person);
+
